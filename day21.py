19a827a9c72ff8be566916b9a70860317c8e7b78
David Wetterau <davidw@dropbox.com>
2021-12-21 10:50:43 -0500

    day 21 part 2

--- day21.py
+++ day21.py
@@ -4,29 +4,35 @@ from collections import defaultdict
 from typing import List, Tuple, Optional
 
 
-die = [0]
-rolls = [0]
+sum_to_count = defaultdict(int)
 
-def roll():
-    rolls[0] += 1
-    x = die[0]
-    die[0] = (die[0] + 1) % 100
-    return x + 1
+def turn(player_is_p0, pos, scores):
+    p = 0 if player_is_p0 else 1
+    op = (p + 1) % 2
+    if scores[op] >= 21:
+        return {op: 1}
+    
+    to_ret = defaultdict(int)
+    for moves, ways in sum_to_count.items():
+        if sum(scores) == 0:
+            print(moves, ways, to_ret)
+        new_pos = [x for x in pos]
+        new_pos[p] = (new_pos[p] + moves) % 10
+        new_scores = [x for x in scores]
+        new_scores[p] += new_pos[p] + 1
+        res = turn(not player_is_p0, new_pos, new_scores)
+        for k, v in res.items():
+            to_ret[k] += ways * v
+    return to_ret
+
 
 def main():
-    pos = [8, 3]
-    scores = [0, 0]
-
-    while True:
-        for p in range(2):
-            moves = roll() + roll() + roll()
-            pos[p] = (pos[p] + moves) % 10
-            scores[p] += pos[p] + 1
-
-            if scores[p] >= 1000:
-                print(rolls[0] * scores[(p+1)%2])
-                sys.exit(0)
-        
+    global sum_to_count
+    for r1 in [1,2,3]:
+        for r2 in [1,2,3]:
+            for r3 in [1,2,3]:
+                sum_to_count[sum((r1, r2, r3))] += 1
+    print(turn(True, [8, 3], [0, 0]))
 
 if __name__ == "__main__":
     main()
